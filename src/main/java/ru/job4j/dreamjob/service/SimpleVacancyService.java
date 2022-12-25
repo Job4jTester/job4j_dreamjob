35435bad6cda6d607ec59dc646ba6e55e6bc9087
ShamRail <rail1999g102r@mail.ru>
2022-12-25 15:33:58 +0500

    2. Связь слоев через Spring DI. [#504856]

---
+++ src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
@@ -0,0 +1,44 @@
+package ru.job4j.dreamjob.service;
+
+import org.springframework.stereotype.Service;
+import ru.job4j.dreamjob.model.Vacancy;
+import ru.job4j.dreamjob.repository.VacancyRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class SimpleVacancyService implements VacancyService {
+
+    private final VacancyRepository vacancyRepository;
+
+    public SimpleVacancyService(VacancyRepository vacancyRepository) {
+        this.vacancyRepository = vacancyRepository;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        return vacancyRepository.save(vacancy);
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return vacancyRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancyRepository.update(vacancy);
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return vacancyRepository.findById(id);
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancyRepository.findAll();
+    }
+
+}
